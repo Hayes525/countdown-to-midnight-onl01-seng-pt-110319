c0b2530854c7799db284b117f1a44ba44379c879
Jordan Hayes <nosafoundation@gmail.com>
2019-11-10 05:42:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,16 @@
 
-def countdown("")
-  countdown(10) 
-  while countdown >= 0
-    puts "#{countdown} SECOND(S)!"
-    countdown -= 1
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0 
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0 
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 0 
+  end 
+  "HAPPY NEW YEAR!"
+end 
